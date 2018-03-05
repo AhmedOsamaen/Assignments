6eb958e9a03df43b1e7e77511c61416f9404d7a3
AhmedOsama <32772828+AhmedOsamaen@users.noreply.github.com>
2018-03-05 17:56:02 +0200

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head><title>Movie Report Using api</title>
+
+</head>
+
+<body>
+
+
+	<center><br><br>
+		<form method="GET" action="Get.php">
+		<h1>Enter movie name</h1>
+			<input type="text" name="t" >
+			<input type="submit" name="search">
+			<h2>Enter movie imdb ID</h2>
+			<input type="text" name="i" >
+			<input type="submit" name="submit">
+		</form>
+	</center>
+</body>
+</html>
